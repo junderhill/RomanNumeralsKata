3648f01ca2aa107cd99a4088c8603cda4b566776
Jason Underhill <jay.underhill@gmail.com>
2015-06-12 13:24:11 +0100

    Create project

---
+++ RomanNumeralsKata.Tests/TestRomanNumeralsKata.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RomanNumeralsKata.Tests
+{
+    [TestClass]
+    public class TestRomanNumeralsKata
+    {
+        [TestMethod]
+        public void TestThatIReturns1()
+        {
+            //arrange
+
+            //act
+
+            //assert
+        }
+    }
+}
